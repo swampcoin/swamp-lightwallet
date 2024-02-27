cda8693dbcc68c21fbbce30bf51eb08d5ec4859e
vecocoin community <155781737+vecocoin@users.noreply.github.com>
2024-02-27 23:14:13 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SWAMP Lightwallet',
+    version='1.0.0',
+    packages=find_packages(),
+    url='vecocoin.com',
+    license='MIT',
+    author='Swamp Coin Community Team',
+    description='Lightwallet Client for SWAMP'
+)
